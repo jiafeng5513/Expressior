20a5cdab0dfbf23836714dfd78ec83fbeda8f748
Anna <jiafeng5513@outlook.com>
2018-05-23 22:38:21 +0800

    ImageProcess is ok

---
+++ src/Libraries/ImageProcess/Converter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Runtime;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Point = Autodesk.DesignScript.Geometry.Point;
+
+/*
+ * Mat-Bitmap转换器
+ */
+namespace ImageProc
+{
+    public class Converter 
+    {
+        private Converter(){}
+
+        /// <summary>
+        /// 利用Emgu的Image转换构造函数将bitmap转换为mat
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        public static Mat BitmapToMat(Bitmap bitmap)
+        {
+            Image<Bgr, Byte> currentFrame = new Image<Bgr, Byte>(bitmap);
+            Mat invert = new Mat();
+            CvInvoke.BitwiseAnd(currentFrame, currentFrame, invert);
+            return invert;
+        }
+
+        /// <summary>
+        /// Mat转换为BitMap
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <returns></returns>
+        public static Bitmap MatToBitmap(Mat mat)
+        {
+            return mat.Bitmap;
+        }
+
+
+    }
+}
---
+++ src/Libraries/ImageProcess/Processor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Analysis;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Runtime;
+using ImageProcess.Properties;
+using Dynamo.Graph.Nodes;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Color = DSCore.Color;
+using Math = DSCore.Math;
+using Point = Autodesk.DesignScript.Geometry.Point;
+/*
+ * 内置节点API
+ * 1.public函数可见，标记为 [IsVisibleInDynamoLibrary(false)]的不可见。
+ * 2.非静态方法需要一个额外的参数this
+ * 3.静态构造器在构造器组里面显示，图标是加号
+ * 4.普通静态函数在普通方法中显示，图标是笔
+ * 5.layoutSpecs.json引用路径：dll名.命名空间名.类名.方法名
+ * 6.添加图标：对应的resx中，嵌入，
+ *      命名规则：类名.方法名.Large/类名.方法名.Small
+ *      大图标：96×96像素，位深32，Alpha,png
+ *      小图标：32×32像素，位深32，Alpna,png
+ */
+namespace ImageProc
+{
+    public class Processor 
+    {
+        /// <summary>
+        /// 关闭合成构造方法
+        /// </summary>
+        private Processor(){}
+        /// <summary>
+        /// sobel边缘检测
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <returns></returns>
+        public static Mat Sobel(Mat mat)
+        {
+            Mat outMat=new Mat();
+            CvInvoke.Canny(mat, outMat, 90, 120, 3);
+            return outMat;
+        }
+        /// <summary>
+        /// 高斯模糊
+        /// </summary>
+        /// <param name="mat"></param>
+        /// <returns></returns>
+        public static Mat GaussianBlur(Mat mat)
+        {
+            Mat outMat = new Mat();
+            CvInvoke.GaussianBlur(mat, outMat,new Size(3,3),0.5,0.5);
+            return outMat;
+        }
+    }
+}
